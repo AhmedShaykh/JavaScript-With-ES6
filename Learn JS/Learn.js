12787d1ae464e80d6049760236038a1d1f712a77
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-04 01:42:03 +0500

    Update Learn.js

--- Learn JS/Learn.js
+++ Learn JS/Learn.js
@@ -552,8 +552,10 @@ function nextInLine(arr, item) {
 
 var testArr = [1, 2, 3, 4, 5];
 
+console.log(testArr);
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
+console.log(testArr);
 console.log("After: " + JSON.stringify(testArr));
 
 // var someObj = {

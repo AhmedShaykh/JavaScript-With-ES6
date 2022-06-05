fc6526c9c965aca16c023317aaae1485fd941b03
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-05 21:54:28 +0500

    It's Time To Push !

--- Topic JS/mainAsync.js
+++ Topic JS/mainAsync.js
@@ -8,9 +8,13 @@ function Greet(msg, alertGreet) {
         Evening(alertGreet);
     } else if (msg === 'morning') {
         Morning(alertGreet);
-    } else {
+    } else if (msg === 'night') {
         Night(alertGreet);
-    };
+    } else {
+        alert('404')
+        console.log('Sorry! This Value Not A Exist');
+        }
+    ;
 };
 
 function Evening(alertGreet) {
@@ -25,11 +29,12 @@ function Morning(alertGreet) {
 
 function Night(alertGreet) {
     alert('Good Night');
-    alertGreet('Go To Rest');
+    alertGreet('Go To Sleep');
 };
 
 function alertGreet(msg) {
     console.log(msg);
 };
 
-Greet('morning', alertGreet);
+var input = prompt("Enter Parts Of Days in Lower Case", "Please Put It")
+Greet(input, alertGreet);

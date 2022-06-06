2e6a6d38d873341f413e18f8fd871537e3d78a37
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-07 02:06:46 +0500

    It's Time To Push !

--- Topic JS/mainNestedLoop.js
+++ Topic JS/mainNestedLoop.js
@@ -8,28 +8,28 @@ for (a = 1; a <= 100; a = a + 10) {
 
 for (var a = 1; a <= 5; a++) {
     for (var b = 1; b <= a; b++) {
-        document.write(b + ' ');
+        document.write('<b>' + b + '</b>');
     };
-    document.write('<br>');
+    document.write('<br> <br>');
 };
 
 for (var i = 1; i <= 5; i++) {
     for (var x = 1; x <= i; x++) {
-        document.write(i, ' ');
+        document.write('<b>' + i + '</b>');
     };
-    document.write('<br>');
+    document.write('<br> <br>');
 };
 
 for (var y = 5; y >= 1; y--) {
     for (var z = 1; z <= y; z++) {
-        document.write(y, ' ');
+        document.write('<b>' + y + '</b>');
     };
-    document.write('<br>');
+    document.write('<br> <br>');
 };
 
 for (var c = 5; c >= 1; c--) {
     for (var d = c; d >= 1; d--) {
-        document.write(d, ' ');
+        document.write('<b>' + c + '</b>');
     };
-    document.write('<br>');
+    document.write('<br> <br>');
 };

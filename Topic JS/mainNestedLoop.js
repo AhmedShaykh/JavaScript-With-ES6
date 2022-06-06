fc90ef4251c2499582668a3abbbe922e4e36c533
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-07 01:55:41 +0500

    It's Time To Push !

--- Topic JS/mainNestedLoop.js
+++ Topic JS/mainNestedLoop.js
@@ -8,7 +8,7 @@ for (a = 1; a <= 100; a = a + 10) {
 
 for (var a = 1; a <= 5; a++) {
     for (var b = 1; b <= a; b++) {
-        document.write(b + ' '); 
+        document.write(b + ' ');
     };
     document.write('<br>');
 };

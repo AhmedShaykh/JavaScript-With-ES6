d15ce7a95fc8a3b54219e62524922d0825ca43e5
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-08 15:49:17 +0500

    It's Time To Push !

--- Topic JS/mainPromises.js
+++ Topic JS/mainPromises.js
@@ -19,96 +19,96 @@ promises().then(() => {
     console.log('Your crush rejected your proposal 💔')
 });
 
-function promise() {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            reject(new Error('Whoops!'));
-        }, 1000);
-    });
-};
+// function promise() {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             reject(new Error('Whoops!'));
+//         }, 1000);
+//     });
+// };
 
-promise().then(
-    result => console.log(result),
-    error => console.log(error)
-);
+// promise().then(
+//     result => console.log(result),
+//     error => console.log(error)
+// );
 
-new Promise(function(resolve, reject) {
-    setTimeout(() => {
-        resolve('Completed');
-    }, 3000);
-})
-    .finally(() => console.log('Done'))
-    .then(result => console.log(result))
+// new Promise(function(resolve, reject) {
+//     setTimeout(() => {
+//         resolve('Completed');
+//     }, 3000);
+// })
+//     .finally(() => console.log('Done'))
+//     .then(result => console.log(result))
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+// function delay(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
   
-delay(3000).then(() => console.log('runs after 3 seconds'));
+// delay(3000).then(() => console.log('runs after 3 seconds'));
 
 
-class Thenable {
-    constructor(num) {
-        this.num = num        
-    };
-    then(resolve, reject) {
-        console.log(resolve)
-        setTimeout(() => resolve(this.num * 2), 1000);
-    };
-};
+// class Thenable {
+//     constructor(num) {
+//         this.num = num        
+//     };
+//     then(resolve, reject) {
+//         console.log(resolve)
+//         setTimeout(() => resolve(this.num * 2), 1000);
+//     };
+// };
 
-new Promise(resolve => resolve(1))
-    .then(result => {
-        return new Thenable(result)
-    })
-    .then(console.log());
-
-
-
-fetch('/article/promise-chaining/user.json')
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(user) {
-        console.log(user.name)
-    });
+// new Promise(resolve => resolve(1))
+//     .then(result => {
+//         return new Thenable(result)
+//     })
+//     .then(console.log());
 
 
-fetch('/article/promise-chaining/user.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(user) {
-        fetch(`https://api.github.com/users/${user.name}`)
-    })
-    .then(response => response.json())
-    .then(function(githubUser) {
-        let img = document.createElement('img');
-        img.src = githubUser.avatar_url;
-        img.className = "promise-avatar-example";
-        document.body.append(img);
 
-        setTimeout(() => {
-            img.remove()
-        }, 3000)
-    });
+// fetch('/article/promise-chaining/user.json')
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(user) {
+//         console.log(user.name)
+//     });
 
 
-async function getData() {
-    let promise01 = new Promise(function(resolve, reject) {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(result) {
-            resolve(result)
-        })
-        .catch(function(error) {
-            reject(error)
-        })
-    })
-    let data = await promise01
-    console.log(data)
-};
+// fetch('/article/promise-chaining/user.json')
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(user) {
+//         fetch(`https://api.github.com/users/${user.name}`)
+//     })
+//     .then(response => response.json())
+//     .then(function(githubUser) {
+//         let img = document.createElement('img');
+//         img.src = githubUser.avatar_url;
+//         img.className = "promise-avatar-example";
+//         document.body.append(img);
 
-getData();
+//         setTimeout(() => {
+//             img.remove()
+//         }, 3000)
+//     });
+
+
+// async function getData() {
+//     let promise01 = new Promise(function(resolve, reject) {
+//         fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(function(response) {
+//             return response.json()
+//         })
+//         .then(function(result) {
+//             resolve(result)
+//         })
+//         .catch(function(error) {
+//             reject(error)
+//         })
+//     })
+//     let data = await promise01
+//     console.log(data)
+// };
+
+// getData();

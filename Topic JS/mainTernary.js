9ea29efeee62f4292bd00cb4b68fd8a817b03ee2
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-08 15:44:25 +0500

    It's Time To Push !

--- Topic JS/mainTernary.js
+++ Topic JS/mainTernary.js
@@ -1,12 +1,12 @@
 var x = 15;
-var z = prompt('Enter Value');
+var z;
 
-z = (x < 10) ? 'True' : 'False';
+z = (x >= 10) ? 'True' : 'False';
 
 console.log(z);
 
-var y = 20;
+var y = prompt('Enter Value');
 var b;
 
-b = "Value is " + ( y == 20 ? 'True' : 'False' );
+b = "Value is " + (y == 20 ? 'True' : 'False');
 console.log(b)

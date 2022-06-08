72a06e8f99050373df33fa679186683903d6ca00
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-08 12:52:34 +0500

    It's Time To Push !

--- Topic JS/mainStatementsIfElse.js
+++ Topic JS/mainStatementsIfElse.js
@@ -1,14 +1,16 @@
-var a = 15;
+var a = prompt('Enter A Value');
 var b = 20;
 
 if (a < b) {
-    console.log('A is greater');
+    console.log('B is Greater Of A');
+} else {
+    console.log('A is Greater Of B');
 };
 
-var per = 60;
+var per = prompt('Enter Your Value', '99 Value Limit Otherwise Value Acceptable');
 
-if (per >= 80 && per  <= 100) {
-    console.log('You Earn A1+ Grade');
+if (per >= 80 && per < 100) {
+    console.log('You Earn A+ Grade');
 } else if (per >= 70 && per < 80) {
     console.log('You Earn A Grade');
 } else if (per >= 60 && per < 70) {
@@ -17,8 +19,10 @@ if (per >= 80 && per  <= 100) {
     console.log('You Earn C Grade');
 } else if (per >= 40 && per < 50) {
     console.log('You Earn D Grade');
-} else if (per >= 30 && per < 40) {
+} else if (per >= 33 && per < 40) {
     console.log('You Earn E Grade');
+} else if (per >= 100) {
+    console.log('Error 404');
 } else {
-    console.log('You are not qualified');
+    console.log('You are not Qualified');
 };

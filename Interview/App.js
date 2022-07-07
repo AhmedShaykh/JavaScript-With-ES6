f8900838bd97c845b7355c374c9ce1fb4ca48bc4
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-07-07 15:11:03 +0500

    It's Time To Push !

--- Interview/App.js
+++ Interview/App.js
@@ -5,201 +5,201 @@
 console.log("a defined? " + (typeof a !== 'undefined'));
 console.log("b defined? " + (typeof b !== 'undefined'));
 
-var myObject = {
-    foo: "bar",
-    func: function () {
-        var self = this;
-        console.log("outer func:  this.foo = " + this.foo); // bar
-        console.log("outer func:  self.foo = " + self.foo); // bar
-        (function () {
-            console.log("inner func:  this.foo = " + this.foo); // undefined
-            console.log("inner func:  self.foo = " + self.foo); // bar
-        }());
-    }
-};
-myObject.func();
+// var myObject = {
+//     foo: "bar",
+//     func: function () {
+//         var self = this;
+//         console.log("outer func:  this.foo = " + this.foo); // bar
+//         console.log("outer func:  self.foo = " + self.foo); // bar
+//         (function () {
+//             console.log("inner func:  this.foo = " + this.foo); // undefined
+//             console.log("inner func:  self.foo = " + self.foo); // bar
+//         }());
+//     }
+// };
+// myObject.func();
 
-function reverseStr(str) {
-    const arr = str.split("");
-    arr.reverse();
-    str = arr.join("");
-    console.log(str);
-};
-reverseStr("Ahmed");
+// function reverseStr(str) {
+//     const arr = str.split("");
+//     arr.reverse();
+//     str = arr.join("");
+//     console.log(str);
+// };
+// reverseStr("Ahmed");
 
-const palindrome = (str) => {
-    let x = str
-    console.log("word before", x)
-    const arr = x.split("");
-    arr.reverse();
-    x = arr.join("")
-    console.log("word after", x);
-    if (x == str) {
-        console.log(true)
-    } else {
-        console.log(false)
-    }
-}
+// const palindrome = (str) => {
+//     let x = str
+//     console.log("word before", x)
+//     const arr = x.split("");
+//     arr.reverse();
+//     x = arr.join("")
+//     console.log("word after", x);
+//     if (x == str) {
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
 
-palindrome('civics');
-
-
-// closures
-
-var num = 3;
-
-function outer() {
-    var num = 2;
-    function inner() {
-        num++
-        var num = 4;
-        console.log(num);
-    }
-    inner();
-};
-
-outer();
-
-console.log(typeof typeof 1);
-
-var hero = {
-    _name: 'John Doe',
-    getSecretIdentity: function () {
-        return this._name
-    }
-};
-
-var identity = hero.getSecretIdentity();
-console.log(identity);
-console.log(hero.getSecretIdentity())
+// palindrome('civics');
 
 
-function myArr(arr, callback) {
-    arr.push(100);
-    callback();
-};
+// // closures
 
-var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// var num = 3;
 
-myArr(arr, function () {
-    console.log('New Number is pushed in array', arr)
-});
+// function outer() {
+//     var num = 2;
+//     function inner() {
+//         num++
+//         var num = 4;
+//         console.log(num);
+//     }
+//     inner();
+// };
 
-function revString(str) {
-    const arr = str.split("");
-    arr.reverse();
-    str = arr.join("");
-    console.log(str)
-};
+// outer();
 
-reverseStr("Hello Bro");
+// console.log(typeof typeof 1);
 
-var arrL = [1, 2, 3, 4, 5, 6];
-var newArr = arrL
-arrL.length = 0
-console.log(newArr);
+// var hero = {
+//     _name: 'John Doe',
+//     getSecretIdentity: function () {
+//         return this._name
+//     }
+// };
 
-
-function checkInt(int) {
-    typeof int === "number" ? console.log("its a number") : console.log("its not an integer")
-};
-
-checkInt(2);
+// var identity = hero.getSecretIdentity();
+// console.log(identity);
+// console.log(hero.getSecretIdentity())
 
 
-var Stack = ["Cat", "Dog", "Eagle"];
+// function myArr(arr, callback) {
+//     arr.push(100);
+//     callback();
+// };
 
-function duplicate(arr) {
-    return arr.concat(arr)
-};
+// var arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 
-console.log(duplicate(Stack));
+// myArr(arr, function () {
+//     console.log('New Number is pushed in array', arr)
+// });
 
+// function revString(str) {
+//     const arr = str.split("");
+//     arr.reverse();
+//     str = arr.join("");
+//     console.log(str)
+// };
 
-function mul(x) {
-    return function (y) {
-        return function (z) {
-            return (x * y * z)
-        }
-    }
-}
+// reverseStr("Hello Bro");
 
-console.log(mul(2)(2)(2));
-
-
-function baseNumber(num) {
-    return function (N) {
-        return num + N
-    }
-};
-var baseResult = baseNumber(6);
-console.log(baseResult(10))
-
-function plus(num) {
-    return function (N) {
-        return num + N
-    };
-};
-
-console.log(plus(3)(3));
+// var arrL = [1, 2, 3, 4, 5, 6];
+// var newArr = arrL
+// arrL.length = 0
+// console.log(newArr);
 
 
-var ary = [1, 2, 3, 4];
-ary.map((y) => {
-    console.log(y * 2)
-});
+// function checkInt(int) {
+//     typeof int === "number" ? console.log("its a number") : console.log("its not an integer")
+// };
 
-const obj = [
-    { fName: 'Syed', lName: 'Bilal' },
-    { fName: 'Mike', lName: 'David' },
-    { fName: 'White', lName: 'Mike' },
-];
-
-obj.map((ob) => {
-    console.log(ob.fName)
-});
-
-const number = [1, -2, 3, -9, 0, 2];
-
-var vn = number.filter((value) => {
-    return value >= 0
-})
-
-console.log(vn);
+// checkInt(2);
 
 
-var res = [
-    { name: 'Broadway', isOpen: true },
-    { name: 'PizzaHut', isOpen: false },
-    { name: 'Wireless', isOpen: true },
-    { name: 'California', isOpen: false },
-];
+// var Stack = ["Cat", "Dog", "Eagle"];
 
-var openRes = res.map(rp => {
-    return rp.name
-})
+// function duplicate(arr) {
+//     return arr.concat(arr)
+// };
 
-console.log(openRes)
+// console.log(duplicate(Stack));
 
-function promise() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let api = false
 
-            if (!api) {
-                console.log('Api Connected');
-                resolve()
-            } else {
-                console.log('Api not Connected');
-                reject()
-            }
-        }, 2000);
-    })
-};
-promise()
-    .then(() => {
-        console.log('Incoming Data')
-    })
-    .catch((error) => {
-        console.log(error)
-    });
+// function mul(x) {
+//     return function (y) {
+//         return function (z) {
+//             return (x * y * z)
+//         }
+//     }
+// }
+
+// console.log(mul(2)(2)(2));
+
+
+// function baseNumber(num) {
+//     return function (N) {
+//         return num + N
+//     }
+// };
+// var baseResult = baseNumber(6);
+// console.log(baseResult(10))
+
+// function plus(num) {
+//     return function (N) {
+//         return num + N
+//     };
+// };
+
+// console.log(plus(3)(3));
+
+
+// var ary = [1, 2, 3, 4];
+// ary.map((y) => {
+//     console.log(y * 2)
+// });
+
+// const obj = [
+//     { fName: 'Syed', lName: 'Bilal' },
+//     { fName: 'Mike', lName: 'David' },
+//     { fName: 'White', lName: 'Mike' },
+// ];
+
+// obj.map((ob) => {
+//     console.log(ob.fName)
+// });
+
+// const number = [1, -2, 3, -9, 0, 2];
+
+// var vn = number.filter((value) => {
+//     return value >= 0
+// })
+
+// console.log(vn);
+
+
+// var res = [
+//     { name: 'Broadway', isOpen: true },
+//     { name: 'PizzaHut', isOpen: false },
+//     { name: 'Wireless', isOpen: true },
+//     { name: 'California', isOpen: false },
+// ];
+
+// var openRes = res.map(rp => {
+//     return rp.name
+// })
+
+// console.log(openRes)
+
+// function promise() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let api = false
+
+//             if (!api) {
+//                 console.log('Api Connected');
+//                 resolve()
+//             } else {
+//                 console.log('Api not Connected');
+//                 reject()
+//             }
+//         }, 2000);
+//     })
+// };
+// promise()
+//     .then(() => {
+//         console.log('Incoming Data')
+//     })
+//     .catch((error) => {
+//         console.log(error)
+//     });
